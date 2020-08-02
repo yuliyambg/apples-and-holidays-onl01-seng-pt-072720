15b1f08f249f78cf86061c179cb196c91c5361b6
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-02 16:13:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 
 holiday_hash.[:winteer].each do |holiday, supplies|
+  supplies<<supply
   
 end
 
